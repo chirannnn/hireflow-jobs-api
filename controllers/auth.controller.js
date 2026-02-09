39a657d7c02877e0cd13bde8d5596332642c2c5e
chirannnn <chiranyt117@gmail.com>
2026-02-10 00:04:51 +0530

    update register-user

---
+++ controllers/auth.controller.js
@@ -0,0 +1,51 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const checkExistingUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (checkExistingUser) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "User is already exists either with same username or same email. Please try with a different username or email",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully!",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+module.exports = { registerUser };
--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 
 const connectToDB = require("./database/db");
 
+const authRoutes = require("./routes/auth.routes");
+
 app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
